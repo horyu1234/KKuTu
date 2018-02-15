72e0e69ce86bd463c7fbea4b59654f1f7bc5fe1b
horyu1234 <horyu1234@naver.com>
2018-02-15 18:10:56 +0900

    [미등록] 채널 접속 로그에서 시간 TimeZone 이 이상한 문제 수정

--- Server/lib/sub/ConnectionLog.js
+++ Server/lib/sub/ConnectionLog.js
@@ -11,11 +11,15 @@ exports.initDatabase = function (_database) {
 };
 
 function addLog(id, name, ip, channel, useragent, fingerprint2) {
-    let time = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
+    let currentTime = new Date();
+    currentTime.setHours(currentTime.getHours() + 9);
+    currentTime.toISOString().replace(/T/, ' ').replace(/\..+/, '');
+
+    let timeText = currentTime.toISOString().replace(/T/, ' ').replace(/\..+/, '');
     let query = "INSERT INTO " + CONNECTION_LOG_TABLE_NAME + " " +
         "VALUES ($1, $2, $3, $4, $5, $6, $7)";
 
-    database.query(query, [time, id, name, ip, channel, useragent, fingerprint2], (err, result) => {
+    database.query(query, [timeText, id, name, ip, channel, useragent, fingerprint2], (err, result) => {
         if (err) {
             return JLog.error(`Error executing query ${err.stack}`);
         }
