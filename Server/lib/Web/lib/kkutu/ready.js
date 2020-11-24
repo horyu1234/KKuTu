dc57bf41088d9ddb4e54b459f8dbeef63f89e45a
Preta Crowz <pretacrowz@gmail.com>
2020-11-24 19:23:32 +0900

    Revert "방장 정보 보기 버튼"
    This reverts commit 5a3c62f83d47c2f01e8dd4635f6dcf57c6e35bbf.

--- Server/lib/Web/lib/kkutu/ready.js
+++ Server/lib/Web/lib/kkutu/ready.js
@@ -97,7 +97,6 @@ $(document).ready(function () {
             inviteRobot: $("#invite-robot"),
             roomInfo: $("#RoomInfoDiag"),
             roomInfoJoin: $("#room-info-join"),
-            roomInfoMaster: $("#room-info-master"),
             profile: $("#ProfileDiag"),
             profileShut: $("#profile-shut"),
             profileHandover: $("#profile-handover"),
@@ -810,9 +809,6 @@ $(document).ready(function () {
     $stage.dialog.roomInfoJoin.on('click', function (e) {
         $stage.dialog.roomInfo.hide();
         tryJoin($data._roominfo);
-    });
-    $stage.dialog.roomInfoMaster.on('click', function (e) {
-        requestProfile($data.rooms[$data._roominfo].master)
     });
     $stage.dialog.profileHandover.on('click', function (e) {
         if (!confirm(L['sureHandover'])) return;
