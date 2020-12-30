440c803822785bd525b538690d7fcc49b4de074d
horyu1234 <horyu1234@naver.com>
2020-12-30 20:16:12 +0900

    세미콜론 수정, 닉네임 변경 가능 일자 확인이 항상 실패하는 문제 수정, 현재와 동일한 닉네임으로 변경 시도 대응 처리

--- Server/lib/sub/UserNickChange.js
+++ Server/lib/sub/UserNickChange.js
@@ -1,85 +1,85 @@
 /**
  * Created by kdhkr on 2020-12-29.
  */
-const JLog = require('./jjlog')
-const DB = require('../Web/db')
+const JLog = require('./jjlog');
+const DB = require('../Web/db');
 
-const nickConf = require('./nick.json')
+const nickConf = require('./nick.json');
 
-const pattern = RegExp(nickConf.pattern['pattern'], nickConf.pattern['flags']['pattern'])
-const bad = RegExp(nickConf.pattern['bad'], nickConf.pattern['flags']['bad'])
-const black = RegExp(nickConf.pattern['black'], nickConf.pattern['flags']['black'])
-const sPattern = RegExp(nickConf.pattern['similarity'], nickConf.pattern['flags']['similarity'])
+const pattern = RegExp(nickConf.pattern['pattern'], nickConf.pattern['flags']['pattern']);
+const bad = RegExp(nickConf.pattern['bad'], nickConf.pattern['flags']['bad']);
+const black = RegExp(nickConf.pattern['black'], nickConf.pattern['flags']['black']);
+const sPattern = RegExp(nickConf.pattern['similarity'], nickConf.pattern['flags']['similarity']);
 
-const nickMin = nickConf.nick['min']
-const nickMax = nickConf.nick['max']
+const nickMin = nickConf.nick['min'];
+const nickMax = nickConf.nick['max'];
 
-const term = nickConf.nick['term'] * 24 * 60 * 60 * 1000
+const term = nickConf.nick['term'] * 24 * 60 * 60 * 1000;
 
-function processUserNickChange(userId, userNick, callback) {
+const processUserNickChange = (userId, userNick, callback) => {
     if (!userId || !userNick) {
-        callback(400)
+        callback(400);
         return
     }
 
     const length = userNick.length
-    if (length < nickMin || length > nickMax || length == 0) {
-        callback(600)
+    if (length < nickMin || length > nickMax || length === 0) {
+        callback(600);
         return
     }
     if (!userNick.match(pattern)) {
-        callback(601)
+        callback(601);
         return
     }
 
     if (userNick.match(bad)) {
-        callback(602)
+        callback(602);
         return
     }
 
     if (userNick.match(black)) {
-        callback(603)
+        callback(603);
         return
     }
 
-    DB.users.findOne(['_id', userId]).on(function($body) {
-        const currentNick = $body.nickname
-        const meanableNick = userNick.replace(sPattern, '')
+    DB.users.findOne(['_id', userId]).on(function ($body) {
+        const currentNick = $body.nickname;
+        const meanableNick = userNick.replace(sPattern, '');
 
-        const date = Date.now()
+        const date = Date.now();
+
+        if (currentNick === userNick) {
+            callback(610)
+            return;
+        }
 
         if (!!$body.isLimitModifyNick) {
-            callback(610)
-            return
+            callback(611)
+            return;
         }
 
-        if (!!$body.lastModifiedNickAt && !checkUserNickChangeTime($body.lastModifiedNickAt)) {
-            callback(611)
-            return
+        if (!isChangeableNickname($body.lastModifiedNickAt)) {
+            callback(612)
+            return;
         }
-        
-        DB.users.findOne(['meanableNick', meanableNick]).on(function($body) {
-            if ($body) {
-                callback(620)
-                return
-            }
+
+        DB.users.findOne(['meanableNick', meanableNick]).on(function ($body) {
+            if ($body) callback(620);
         })
 
-        DB.users.update(['_id', userId]).set([ 'nickname', userNick ], [ 'meanableNick', meanableNick ], [ 'lastModifiedNickAt', date ]).on()
+        DB.users.update(['_id', userId]).set(['nickname', userNick], ['meanableNick', meanableNick], ['lastModifiedNickAt', date]).on();
 
-        JLog.info(`[NICK] ${userId}님이 닉네임을 변경하였습니다. 기존: ${currentNick} / 신규: ${userNick}`)
+        JLog.info(`[NICK] ${userId}님이 닉네임을 변경하였습니다. 기존: ${currentNick} / 신규: ${userNick}`);
 
-        callback(630)
+        callback(630);
     })
-    return null
 }
 
-function checkUserNickChangeTime(nickChangeTime) {
-    if (nickChangeTime !== undefined) return
-    if (isNaN(parseInt(nickChangeTime))) return
-    if (nickChangeTime + term > Date.now()) return
-    
-    return true
+const isChangeableNickname = (nickChangeTime) => {
+    const number = parseInt(nickChangeTime);
+    return nickChangeTime === undefined
+        || isNaN(number)
+        || number + term < Date.now();
 }
 
-exports.processUserNickChange = processUserNickChange
+exports.processUserNickChange = processUserNickChange;
