dea67bebba93b32485b6dbb9a96be4b383ed41a8
horyu1234 <horyu1234@naver.com>
2020-12-30 18:00:32 +0900

    ZWNBSP 문자열 제거

--- Server/lib/Game/games/cw_maker.js
+++ Server/lib/Game/games/cw_maker.js
@@ -16,7 +16,7 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-﻿var Prompt = require('prompt');
+var Prompt = require('prompt');
 var DB = require('../../Web/db');
 var Const = require('../../const');
 var Lizard = require('../../sub/lizard');
