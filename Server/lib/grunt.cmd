89e67c4a77756c6426495f3f0cfd79a2d7948ae2
horyu1234 <horyu1234@naver.com>
2020-12-30 17:52:37 +0900

    웹 관련, 서버 콘솔 GUI 관련 불필요한 파일 제거

--- Server/lib/grunt.cmd
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\node_modules\grunt\bin\grunt" %*
-) ELSE (
-  @SETLOCAL
-  @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\node_modules\grunt\bin\grunt" %*
-)
-PAUSE
