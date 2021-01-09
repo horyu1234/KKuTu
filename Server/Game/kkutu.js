665645c6ad46aa68e457839f2344410a352a654b
horyu1234 <horyu1234@naver.com>
2021-01-09 21:05:26 +0900

    GUEST 닉네임 접두사를 "손님" 으로 변경

--- Server/Game/kkutu.js
+++ Server/Game/kkutu.js
@@ -1559,7 +1559,7 @@ function getGuestName(sid) {
     for (i = 0; i < len; i++) {
         res += sid.charCodeAt(i) * (i + 1);
     }
-    return "GUEST" + (1000 + (res % 9000));
+    return "손님" + (1000 + (res % 9000));
 }
 
 function shuffle(arr) {
